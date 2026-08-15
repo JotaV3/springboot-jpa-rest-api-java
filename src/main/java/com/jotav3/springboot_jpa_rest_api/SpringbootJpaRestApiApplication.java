e7dc95d448db19a6bfad7bf3923925b9f93a1d58
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-15 02:42:41 -0300

    Initial commit: set up Spring Boot project

---
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/SpringbootJpaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jotav3.springboot_jpa_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotav3/springboot_jpa_rest_api/SpringbootJpaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotav3.springboot_jpa_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
