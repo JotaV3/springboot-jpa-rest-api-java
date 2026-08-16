386765a4cbc952fbc0d3ae6bff5e7e2d75c8cf00
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 19:00:58 -0300

    Implement Product findById endpoint

--- src/main/java/com/jotav3/springboot_jpa_rest_api/services/ProductService.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/services/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,5 +15,10 @@ public class ProductService {
 
     public List<Product> findAll(){
         return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> optional = repository.findById(id);
+        return optional.get();
     }
 }
