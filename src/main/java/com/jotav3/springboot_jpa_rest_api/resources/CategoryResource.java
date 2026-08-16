6e557c78c83933c219bd7a46d34b76cdc9054096
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 14:29:26 -0300

    Implement Category findAll endpoint

--- src/main/java/com/jotav3/springboot_jpa_rest_api/resources/CategoryResource.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/resources/CategoryResource.java
@@ -3,13 +3,23 @@ package com.jotav3.springboot_jpa_rest_api.resources;
 import com.jotav3.springboot_jpa_rest_api.entities.Category;
 import com.jotav3.springboot_jpa_rest_api.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
     @Autowired
     private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 
 }
