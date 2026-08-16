128bf689e6a8d37ae7926520e30de2c38d3f1fda
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 18:40:00 -0300

    Add transient Product and Category associations

--- src/main/java/com/jotav3/springboot_jpa_rest_api/entities/Product.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/entities/Product.java
@@ -18,6 +18,7 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
+    @Transient
     private Set<Category> categories = new HashSet<>();
 
     public Product(){
