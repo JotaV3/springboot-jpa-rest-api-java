094ca477229d183cca9491f42f0db97e457f3ec9
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 18:58:03 -0300

    Implement Product findAll endpoint

--- src/main/java/com/jotav3/springboot_jpa_rest_api/resources/ProductResource.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/resources/ProductResource.java
@@ -1,13 +1,24 @@
 package com.jotav3.springboot_jpa_rest_api.resources;
 
+import com.jotav3.springboot_jpa_rest_api.entities.Product;
 import com.jotav3.springboot_jpa_rest_api.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductResource {
     @Autowired
     private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/jotav3/springboot_jpa_rest_api/services/ProductService.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/services/ProductService.java
@@ -1,11 +1,18 @@
 package com.jotav3.springboot_jpa_rest_api.services;
 
+import com.jotav3.springboot_jpa_rest_api.entities.Product;
 import com.jotav3.springboot_jpa_rest_api.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
 }
