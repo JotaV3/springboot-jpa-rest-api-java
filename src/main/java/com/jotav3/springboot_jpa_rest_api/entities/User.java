780e0c7a227c6826628884661146a34e4b1ec5b4
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 01:13:27 -0300

    Ignore user orders in JSON serialization

--- src/main/java/com/jotav3/springboot_jpa_rest_api/entities/User.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/entities/User.java
@@ -1,5 +1,6 @@
 package com.jotav3.springboot_jpa_rest_api.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orderList;
 
