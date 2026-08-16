7b3a13905a12ef7e42f4c8e57cb03c2168ce14cf
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-16 14:35:49 -0300

    Implement Category findById endpoint

--- src/main/java/com/jotav3/springboot_jpa_rest_api/resources/CategoryResource.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.jotav3.springboot_jpa_rest_api.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,12 @@ public class CategoryResource {
     public ResponseEntity<List<Category>> findAll(){
         List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
     }
 
 }
