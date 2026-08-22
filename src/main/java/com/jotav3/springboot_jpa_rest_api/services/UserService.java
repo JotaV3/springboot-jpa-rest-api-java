f34b5444488bd2c03556caa44be1cf4611063424
joaovictor0123mo@gmail.com <joaovictor0123mo@gmail.com>
2026-08-22 19:00:16 -0300

    Handle user not found in findById endpoint

--- src/main/java/com/jotav3/springboot_jpa_rest_api/services/UserService.java
+++ src/main/java/com/jotav3/springboot_jpa_rest_api/services/UserService.java
@@ -2,6 +2,7 @@ package com.jotav3.springboot_jpa_rest_api.services;
 
 import com.jotav3.springboot_jpa_rest_api.entities.User;
 import com.jotav3.springboot_jpa_rest_api.repositories.UserRepository;
+import com.jotav3.springboot_jpa_rest_api.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> optional = repository.findById(id);
-        return optional.get();
+        return optional.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
